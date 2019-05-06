4b46d0b04fcb00e6f4965d68c579de392ea44fec
zhangnan <zn_god@163.com>
2019-05-06 09:32:58 +0800

    first commit

---
+++ src/main/java/com/shengzhiyi/tea/controller/TeaController.java
@@ -0,0 +1,12 @@
+package com.shengzhiyi.tea.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TeaController {
+
+
+    public static void main(String[] args) {
+
+    }
+}
